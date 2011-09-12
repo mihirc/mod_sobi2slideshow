2ee419ede9cd6427ea42c83f2db0424f8ddf29a3
Mihir Chhatre <cmihirhere@gmail.com>
2011-09-12 11:51:02 +0530

    working fix. problem with mootools.js in joomla

--- mod_sobi2slideshow.php
+++ mod_sobi2slideshow.php
@@ -12,27 +12,19 @@ $select_width = $params->get('select_width');
 $select_time = $params->get('select_time');
 ?>
 <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/modules/mod_sobi2slideshow/NivooSlider.css" />
-<script type="text/javascript" src="<?php echo $this->baseurl ?>/media/system/js/mootools.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/mootools/1.3.2/mootools-yui-compressed.js"></script>
 <script type="text/javascript" src="<?php echo $this->baseurl ?>/modules/mod_sobi2slideshow/NivooSlider.js"></script>
 
 
     <script type="text/javascript">
         window.addEvent('domready', function () {
-			$$('a')[1].addEvent('click', function(e){
-				e.stop();
-				alert('[you triggered a click event]');
-			});
-            new NivooSlider($('Slider'), {
-				effect: 'random',
-				interval: 5000,
-				orientation: 'random'
-			});
-        }); 
+ new NivooSlider($('Slider'));
+         }); 
     </script>
     <?php if ( $option == "com_sobi2" and JRequest::getVar('sobi2Task') == 'sobi2Details' ) { $sobi2Id = JRequest::getVar( 'sobi2Id' ); ?>
 <div id="Wrapper" style="width:<?php echo $select_width; ?>px; height:<?php echo $select_height; ?>px;">
 		<div id="Slider" class="nivoo-slider">
-	    <?php $files = glob('$select_folder/entries/"'.$sobi2Id.'"/*.*'); for ($i=0; $i<count($files); $i++) { $num = $files[$i]; echo '<img src="'.$num.'" alt="random image">'; } ?>
+	    <?php $files = glob('$select_folder/entries/"'.$sobi2Id.'"/*.*'); for ($i=0; $i<count($files); $i++) { $num = $files[$i]; echo '<img src="'.$num.'" alt="random image" title="sobi2slideshow" height="'.$select_height.'" width="'.$select_width.'">'; } ?>
     	</div>
 	</div>   
     <?php } elseif ( $option == 'com_sobi2' and ($catid = JRequest::getVar('catid')) != '' and JRequest::getVar('sobi2Task') == '' ) { ?>
