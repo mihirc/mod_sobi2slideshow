340741abb6380bdf880a276de29095d2d671da02
Mihir Chhatre <cmihirhere@gmail.com>
2011-09-12 12:03:55 +0530

    navigation bar support added

--- mod_sobi2slideshow.php
+++ mod_sobi2slideshow.php
@@ -17,18 +17,27 @@ $select_time = $params->get('select_time');
 
 
     <script type="text/javascript">
-        window.addEvent('domready', function () {
- new NivooSlider($('Slider'));
-         }); 
+        window.addEvent('domready',function(){
+    // The more advanced way
+    new NivooSlider($('Slider'), {
+        animSpeed: 750,
+        effect: 'sliceLeftRightDown',
+        interval: <?php echo $select_time; ?>,
+        orientation: 'horizontal',
+        slices: 20,
+		directionNav:true
+    })
+
+});
     </script>
     <?php if ( $option == "com_sobi2" and JRequest::getVar('sobi2Task') == 'sobi2Details' ) { $sobi2Id = JRequest::getVar( 'sobi2Id' ); ?>
-<div id="Wrapper" style="width:<?php echo $select_width; ?>px; height:<?php echo $select_height; ?>px;">
+<div id="Wrapper" class="item" style="width:<?php echo $select_width; ?>px; height:<?php echo $select_height; ?>px;">
 		<div id="Slider" class="nivoo-slider">
 	    <?php $files = glob('$select_folder/entries/"'.$sobi2Id.'"/*.*'); for ($i=0; $i<count($files); $i++) { $num = $files[$i]; echo '<img src="'.$num.'" alt="random image" title="sobi2slideshow" height="'.$select_height.'" width="'.$select_width.'">'; } ?>
     	</div>
 	</div>   
     <?php } elseif ( $option == 'com_sobi2' and ($catid = JRequest::getVar('catid')) != '' and JRequest::getVar('sobi2Task') == '' ) { ?>
-<div id="Wrapper" style="width:<?php echo $select_width; ?>px; height:<?php echo $select_height; ?>px;">
+<div id="Wrapper" class="category" style="width:<?php echo $select_width; ?>px; height:<?php echo $select_height; ?>px;">
 		<div id="Slider" class="nivoo-slider">
 	    <?php $files = glob("$select_folder/categories/".$catid."/*.*"); for ($i=0; $i<count($files); $i++) { $num = $files[$i]; echo '<img src="'.$num.'" alt="random image">'; } ?>
     	</div>
